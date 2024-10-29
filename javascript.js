72d569f705ae29063bbfd228250120175248f4df
AphiweNgubane <164533620+AphiweNgubane@users.noreply.github.com>
2024-10-29 14:59:32 +0200

    create operate function, executes equation depending on operator input

--- javascript.js
+++ javascript.js
@@ -13,3 +13,27 @@ const multiply = function (num1, num2){
 const divide = function (num1, num2){
     return num1 / num2;
 }
+
+let num1;
+let num2;
+let operator;
+
+const operate = function (num1, operator, num2){
+    if (operator === "add"){
+        add(num1, num2);
+    }
+
+    if (operator === "minus"){
+        subtract(num1, num2);
+    }
+
+    if (operator === "multiply"){
+        multiply(num1, num2);
+    }
+
+    if (operator === "divide"){
+        divide(num1, num2);
+    }
+}
+
+console.log(operate(5, "add", 2))
